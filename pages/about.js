84b0f595b66be4277c37578f683a38bc825e6ef2
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-06-20 21:53:36 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is an app to find the latest DJ and the other musical events</p>
+      <p>version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+const EventPage = () => {
+  return (
+    <div>
+      <h1>My event</h1>
+    </div>
+  );
+};
+
+export default EventPage;
