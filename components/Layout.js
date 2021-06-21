419f53eb063c1858134296eaa3bebf7b02487965
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-06-21 17:38:27 +0300

    added static props , server side props , eventItem

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import styles from '../styles/Layout.module.css';
+import { useRouter } from 'next/router';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Showcase from '@/components/Showcase';
+import styles from '@/styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -11,7 +15,11 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,93 @@
-import Layout from '../../components/Layout';
+import Layout from '@/components/Layout';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/styles/Event.module.css';
+import { API_URL } from '@/config/index';
+export default function EventsPage({ evt }) {
+  const deleteEvent = e => {
+    console.log('delete');
+  };
 
-export default function EventsPage() {
   return (
     <Layout>
-      <h1>My event</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes />
+            Delete Event
+          </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{'< '} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map(evt => ({
+    params: { slug: evt.slug }
+  }));
+  return {
+    paths,
+    fallback: true
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  console.log(slug);
+
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  console.log(events);
+
+  return {
+    props: {
+      evt: events[0]
+    },
+    revalidate: 1
+  };
+}
+
+// export async function getServerSslugeProps({ query: { slug } }) {
+//   console.log(slug);
+
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   console.log(events);
+
+//   return {
+//     props: {
+//       evt: events[0]
+//     }
+//   };
+// }
